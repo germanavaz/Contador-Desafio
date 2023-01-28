01fd7b87a7e2385c8eb1edb0d3db80938e19571d
Germana Vaz <108768134+germanavaz@users.noreply.github.com>
2023-01-28 16:46:36 -0300

    Add files via upload

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Contador {
+
+	public static void main(String[] args) {
+
+		Scanner terminal = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+
+		try {
+			contar(parametroUm, parametroDois);
+		} catch (ParametrosInvalidosException e) {
+		}
+
+		terminal.close();
+
+	}
+
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		if (parametroUm >= parametroDois) {
+			throw new ParametrosInvalidosException();
+		}
+		int contador = parametroDois - parametroUm;
+		for(int i = 0;i<contador;i++)
+			System.out.println("Imprimindo número "+(i+1));
+	}
+
+}
+
+
---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,15 @@
+
+public class ParametrosInvalidosException extends Exception  {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ParametrosInvalidosException() {
+		System.out.println("O primeiro parametro deve ser menor do que o segundo!");
+	}
+
+	
+}
